ce54a5c4f5aff8b460c0247138d7223b1663deb6
Federico Tondolo <51007153+ftondolo@users.noreply.github.com>
2021-06-23 18:34:24 -0400

    Update master.py

--- master.py
+++ master.py
@@ -1,4 +1,4 @@
-import platform, time
+import os, platform, time
 
 os_platform = platform.system()
 
@@ -7,22 +7,26 @@ preset = 100
 
 #Linux 
 if os_platform == 'Linux':
+    preset = str(os.popen('amixer get Capture').readlines()[0])
     while True:
         if mute:
-            print("Linux, muting")
+            os.system('amixer set Capture ' + preset +'%')
             mute = 0
             time.sleep(2)
         else:
+            os.system('amixer set Capture 0%')
             mute = 1
             time.sleep(2)
 #Mac
 elif os_platform == 'Darwin':
+    preset = str(os.popen('osascript -e "input volume of (get volume settings)"').readlines()[0])
     while True:
         if mute:
-            print("Mac, muting")
+            os.system('osascript -e "set volume input volume ' + preset +'"')
             mute = 0
             time.sleep(2)
         else:
+            os.system('osascript -e "set volume input volume 0"')
             mute = 1
             time.sleep(2)
 #Windows
